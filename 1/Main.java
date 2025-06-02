d9179793f3df2850abcd223e3ea8da1cefc4a728
Aminkhishig <aminkhishiga@gmail.com>
2025-06-02 12:09:37 +0800

    anhnii commit

---
+++ 1/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person("Amina", 18);
+
+        System.out.println(person1.getName() + " is " + person1.getAge() + " years old.");
+
+        person1.setAge(19);
+        person1.setName("Aminkhishig");
+
+        System.out.println("Set new age and name:");
+        System.out.println(person1.getName() + " is now " + person1.getAge() + " years old.");
+    }
+} 
